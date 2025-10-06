feb5d1e62ae4a1a9019f44545a210d2964ad38bf
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-06 19:52:30 +0530

    Update common.sh

--- common.sh
+++ common.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
 USERID=$(id -u)
+R="\e[31m"
+G="\e[33m"
 Y="\e[33m"
 N="\e[0m"
 
 LOGS_FOLDER="/var/log/Shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
 mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+check_root(){
+if [ $USERID -ne 0 ]; then
+   echo -e "$R privilege:: Please run this script with root privilege $N"
+   exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+       echo -e "$2 ... $R FAILURE $N"
+       exit 1
+      else
+       echo -e "$2 ... $G SUCCESS $N"
+    fi    
+  }
+}
 
 
 
