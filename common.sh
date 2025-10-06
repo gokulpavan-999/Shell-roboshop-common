838d6bf44f3e0f50aaa845774992481e5aa383d5
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-06 20:24:03 +0530

    Update common.sh

--- common.sh
+++ common.sh
@@ -9,6 +9,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/Shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+START_TIME=$(date +%s)
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
@@ -21,13 +22,16 @@ fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-       echo -e "$2 ... $R FAILURE $N"
+       echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
        exit 1
       else
-       echo -e "$2 ... $G SUCCESS $N"
+       echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi    
   }
 }
 
-
-
+print_total_time(){
+    END_TIME=$(date +%s)
+    TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+    echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
+}
