c0ff146a35cc965f24ef780752247857741b47b2
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-23 13:31:17 +0530

    Update redis.sh

--- redis.sh
+++ redis.sh
@@ -12,7 +12,13 @@ dnf module enable redis:7 -y
 VALIDATE $? "Enabling redis server"
 
 dnf install redis -y
-VALIDATE $? "Installing redis"
+VALIDATE $? "Installing redis server"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "Allowing Remote connections to redis"
+
+systemctl enable redis
+VALIDATE $? "Enabling redis server"
+
+systemctl start redis
+VALIDATE $? "Starting redis server"
